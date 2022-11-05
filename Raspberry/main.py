511f6dd3515f36cb336627df0f82216810f3cc04
Mariia Cherepnina <m.cherepnina@tum.de>
2022-11-05 20:09:16 +0100

    fix Raspberry side functions

--- Raspberry/main.py
+++ Raspberry/main.py
@@ -1,19 +1,26 @@
 import os
 import requests
+from enum import Enum
 import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
 import const
 
 
-class LEDState:
+class LEDState(Enum):
     OFF = 0
     RED = 1
     GREEN = 2
 
 
-class BoxState:
+class BoxState(Enum):
     CLOSED = 0
     OPENED = 1
+
+
+class PersonType(Enum):
+    DELIVERER = 0
+    CUSTOMER = 1
 
 
 class ServerCommunicator:
@@ -22,60 +29,68 @@ class ServerCommunicator:
         self.session = requests.Session()
         self.session.auth = (rasp_name, rasp_pass)
 
-        url = self.server_address  # TODO may by another address
+        url = self.server_address  # TODO may be another address
         response = self.session.get(url, verify=False)
         assert response.status_code == 200, (
             f'Server addr, rasp_name or rasp_pass is incorrect. The GET request to "{url}" with '
             f'user "{rasp_name}" and pass "{rasp_pass}" returned {response.status_code} status code'
         )
 
-    def _call_server(self, url):
+    def check_person_name(self, name: str, p_type: PersonType):
+        if p_type == PersonType.DELIVERER:
+            url = os.path.join(self.server_address, 'other/magic/path/', name)  # TODO
+        else:
+            url = os.path.join(self.server_address, 'third/magic/path/', name)  # TODO
+
         response = self.session.get(url)
         assert response.status_code == 200, \
             f'INFO: The response of the query "{url}" is incorrect, status_code is {response.status_code}'
         return response
 
-    def check_deliverer(self, deliverer_name: str) -> bool:
-        url = os.path.join(self.server_address, 'other/magic/path/', deliverer_name)  # TODO
-        response = self._call_server(url)
-        return response.json()['result']
-
-    def check_customer(self, customer_name: str) -> bool:
-        url = os.path.join(self.server_address, 'third/magic/path/', customer_name)  # TODO
-        response = self._call_server(url)
-        return response.json()['result']
+    def update_box_status(self, message: str) -> None:
+        """send to the backEnd 'The goods were taken' and so on"""
+        pass
 
 
-def init_hardware():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(const.HW_LEG_RED, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(const.HW_LEG_GREEN, GPIO.OUT, initial=GPIO.LOW)
+class HardwareController:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(const.HW_LEG_RED, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(const.HW_LEG_GREEN, GPIO.OUT, initial=GPIO.LOW)
+        self.reader = SimpleMFRC522()
+
+    def read_RFID_name(self):
+        _, text = self.reader.read()
+        return text
+
+    @staticmethod
+    def switch_led(state: LEDState) -> None:
+        if state == LEDState.GREEN:
+            GPIO.output(const.HW_LEG_GREEN, GPIO.HIGH)
+        elif state == LEDState.RED:
+            GPIO.output(const.HW_LEG_RED, GPIO.HIGH)
+        elif state == LEDState.OFF:
+            GPIO.output(const.HW_LEG_GREEN, GPIO.LOW)
+            GPIO.output(const.HW_LEG_RED, GPIO.LOW)
+        else:
+            raise RuntimeError(f"LEADState {state} is not implemented")
+
+    @staticmethod
+    def check_box_state() -> BoxState:
+        result = GPIO.input(const.HW_LEG_SENSOR)  # TODO what dose 'input()' return? Float?
+        if result > 0.5:
+            return BoxState.OPENED
+        else:
+            return BoxState.CLOSED
 
 
-def switch_led(state: LEDState) -> None:
-    if state == LEDState.GREEN:
-        GPIO.output(const.HW_LEG_GREEN, GPIO.HIGH)
-    elif state == LEDState.RED:
-        GPIO.output(const.HW_LEG_RED, GPIO.HIGH)
-    elif state == LEDState.OFF:
-        GPIO.output(const.HW_LEG_GREEN, GPIO.LOW)
-        GPIO.output(const.HW_LEG_RED, GPIO.LOW)
-    else:
-        raise RuntimeError(f"LEADState {state} is not implemented")
-
-
-def check_box_state() -> BoxState:
-    result = GPIO.input(const.HW_LEG_SENSOR)  # TODO what dose 'input()' return? Float?
-    if result > 0.5:
-        return BoxState.OPENED
-    else:
-        return BoxState.CLOSED
-
-
-def update_box_status() -> None:
-    """send to the backEnd 'The goods were taken' and so on"""
-    pass
+def check_RFID_name(self, controller: HardwareController,
+                    communicator: ServerCommunicator,
+                    p_type: PersonType) -> bool:
+    name = controller.read_RFID_name()
+    result = communicator.check_person_name(name=name, p_type=p_type)
+    return result
 
 
 def main_loop():
