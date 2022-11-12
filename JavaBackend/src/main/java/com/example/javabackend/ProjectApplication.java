144034622750883e03ef91bf252d4d9af1c32a2d
Sathwik Amburi <sathwik.amburi@tum.de>
2022-11-12 12:58:43 +0100

    test code

--- JavaBackend/src/main/java/com/example/javabackend/ProjectApplication.java
+++ JavaBackend/src/main/java/com/example/javabackend/ProjectApplication.java
@@ -1,5 +1,7 @@
 package com.example.javabackend;
 
+import com.example.javabackend.model.Project;
+import com.example.javabackend.service.ProjectService;
 import com.mongodb.client.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +11,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+import java.util.List;
+
 @SpringBootApplication
 public class ProjectApplication implements CommandLineRunner {
     private static final Logger log = LoggerFactory.getLogger(ProjectApplication.class);
     @Autowired
     MongoClient mongoClient;
+    @Autowired
+    ProjectService projectService;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
@@ -22,5 +28,25 @@ public class ProjectApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("MongoClient = " + mongoClient.getClusterDescription());
+
+        String projectName = "ASE Delivery";
+
+
+        Project project = projectService.createProject(new Project(projectName));
+
+        log.info(String.format("Project %s is created with id %s",
+                project.getName(),
+                project.getId()));
+
+        Project aseDeliveryProject = projectService.findByName(projectName);
+
+        log.info(String.format("Found Project %s with id %s",
+                project.getName(),
+                project.getId()));
+
+        List<Project> projectList = projectService.getAllProjects();
+        log.info("Number of Project in Database is " + projectList.size());
     }
 }
+
+
