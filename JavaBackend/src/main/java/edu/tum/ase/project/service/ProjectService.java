3b16ad2c83e5a25693e0eb18be4f99cc6f4579dc
ge87heq <m.cherepnina@tum.de>
2022-11-19 20:37:00 +0100

    save project

--- JavaBackend/src/main/java/edu/tum/ase/project/service/ProjectService.java
+++ JavaBackend/src/main/java/edu/tum/ase/project/service/ProjectService.java
@@ -5,6 +5,7 @@ import edu.tum.ase.project.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class ProjectService {
@@ -13,7 +14,7 @@ public class ProjectService {
 
     public Project createProject(Project project) {
         // TODO: implement
-        return null;
+        return projectRepository.save(project);  // Looks like 'save' is a default method of Spring service
     }
 
     public Project findByName(String name) {
