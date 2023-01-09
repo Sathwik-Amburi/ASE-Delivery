500ba05673d21a9b7f9ea0981eec43aaec4c4840
Sathwik Amburi <sathwik.amburi@tum.de>
2023-01-09 22:57:36 +0100

    dispatcher part 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navBar";
 import Login from "./components/login";
 import Register from "./components/register";
-import Dispatcher from "./components/dispatcher/dispatcher";
+import Dispatcher from "./components/dispatcher/Dispatcher";
 
 function App() {
   return (
--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -13,10 +13,10 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import LandingPage from "./landingPage";
 import Copyright from "./copyright";
-
-const theme = createTheme();
+import { Navigate, useNavigate } from "react-router-dom";
 
 export default function SignInSide() {
+  const navigate = useNavigate();
   const [userRole, setUserRole] = useState("dispatcher");
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -26,106 +26,105 @@ export default function SignInSide() {
       password: data.get("password"),
       role: userRole,
     });
+    navigate("/dispatcher");
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid item xs={false} sm={4} md={7}>
-          <LandingPage />
-        </Grid>
-        <Grid item xs={12} sm={8} md={5} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box>
-              <Button
-                variant={userRole === "dispatcher" ? "contained" : "outlined"}
-                onClick={() => setUserRole("dispatcher")}
-              >
-                Dispatcher
-              </Button>
-              <Button
-                variant={userRole === "customer" ? "contained" : "outlined"}
-                onClick={() => setUserRole("customer")}
-              >
-                Customer
-              </Button>
-              <Button
-                variant={userRole === "deliverer" ? "contained" : "outlined"}
-                onClick={() => setUserRole("deliverer")}
-              >
-                Deliverer
-              </Button>
-            </Box>
-            <Box
-              component="form"
-              noValidate={false}
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="/register" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
-          </Box>
-        </Grid>
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7}>
+        <LandingPage />
       </Grid>
-    </ThemeProvider>
+      <Grid item xs={12} sm={8} md={5} elevation={6}>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box>
+            <Button
+              variant={userRole === "dispatcher" ? "contained" : "outlined"}
+              onClick={() => setUserRole("dispatcher")}
+            >
+              Dispatcher
+            </Button>
+            <Button
+              variant={userRole === "customer" ? "contained" : "outlined"}
+              onClick={() => setUserRole("customer")}
+            >
+              Customer
+            </Button>
+            <Button
+              variant={userRole === "deliverer" ? "contained" : "outlined"}
+              onClick={() => setUserRole("deliverer")}
+            >
+              Deliverer
+            </Button>
+          </Box>
+          <Box
+            component="form"
+            noValidate={false}
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Copyright sx={{ mt: 5 }} />
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
