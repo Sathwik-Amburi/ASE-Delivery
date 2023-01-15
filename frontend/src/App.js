654bfe92b219a988605daac7db4681302ad4e7ef
Sathwik Amburi <sathwik.amburi@tum.de>
2023-01-15 15:25:18 +0100

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,25 @@ import Register from "./components/register";
 import Dispatcher from "./components/dispatcherDashboard/Dashboard";
 import Deliverer from "./components/delivererDashboard/Deliverer";
 import Customer from "./components/customerDashboard/Customer";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        setResponse(response);
+        console.log(response);
+      })
+      .catch((error) => {
+        setError(error);
+        console.log(error);
+      });
+  }, []);
   return (
     <>
       <NavBar />
