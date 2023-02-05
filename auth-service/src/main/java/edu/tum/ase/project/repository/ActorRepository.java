113afd6da74d7150f9a662d681865f07e08123ee
jonas <jonas.fromm@tum.de>
2023-02-05 08:26:28 +0100

    add auth service

---
+++ auth-service/src/main/java/edu/tum/ase/project/repository/ActorRepository.java
@@ -0,0 +1,18 @@
+package edu.tum.ase.project.repository;
+
+import edu.tum.ase.project.model.Actor;
+import edu.tum.ase.project.utils.ActorType;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ActorRepository extends MongoRepository<Actor, String> {
+    Optional<Actor> findByActorTypeAndEmail(ActorType actorType, String email);
+
+    List<Actor> findAllByActorType(ActorType actorType);
+
+    Optional<Actor> findByIdAndActorType(String id, ActorType actorType);
+
+    Optional<Actor> findByEmailAndPass(String email, String pass);
+}
---
+++ auth-service/src/test/java/edu/tum/ase/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tum.ase.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
