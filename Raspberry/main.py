b7daa9689e96aa99f941ca1f38d276834434fdbe
jonas <jonas.fromm@tum.de>
2023-02-05 13:40:59 +0100

    first ideas

--- Raspberry/main.py
+++ Raspberry/main.py
@@ -16,11 +16,9 @@ def check_RFID_name(controller: HardwareController,
     print(f'INFO: is the name authorised: {result}')
     return result, order_id
 
-
-def handle_opened(order_id, time_opened, status_str, controller, communicator) -> bool:
+def closed_correctly(time_opened, controller) -> bool:
     while datetime.now() - time_opened <= timedelta(seconds=10):  # fewer than 10s have passed since opening the box
         if controller.check_box_state() == BoxState.CLOSED:
-            communicator.change_order_status(order_id=order_id, status_str=status_str)
             return True
 
         time.sleep(0.25)
@@ -35,19 +33,31 @@ def main_loop():
         box_number=const.BOX_NUMBER)
 
     print('Main loop is running')
+
+    delivered = False
+
     while True:
-        RFID_result, order_id = check_RFID_name(
-            controller=controller, communicator=communicator, p_type=ActorType.DELIVERER)
+        if delivered:
+            RFID_result, order_id = check_RFID_name(
+                controller=controller, communicator=communicator, p_type=ActorType.CLIENT)
+        else:
+            RFID_result, order_id = check_RFID_name(
+                controller=controller, communicator=communicator, p_type=ActorType.DELIVERER)
 
         if RFID_result:
             time_opened = datetime.now()
             controller.switch_led(LEDState.GREEN)
 
-            if not handle_opened(order_id=order_id, time_opened=time_opened, status_str=const.STATUS_ONITSWAY,
-                                 controller=controller, communicator=communicator):
+            if closed_correctly(time_opened=time_opened, controller=controller):
+                status_str = "whatever"
+
+                if delivered:
+                    status_str = "something else"
+
+                communicator.change_order_status(order_id=order_id, status_str=status_str)
+            else: # box was not closed correctly
                 controller.blink_led(LEDState.RED, 5)
                 controller.switch_led(LEDState.OFF)
-            break
 
         else:
             controller.switch_led(LEDState.RED)
@@ -55,29 +65,6 @@ def main_loop():
 
         controller.switch_led(LEDState.OFF)
 
-
-        #  --------------------- the same logic, but for the customer
-
-    time.sleep(5)
-    while True:
-        RFID_result, order_id = check_RFID_name(
-            controller=controller, communicator=communicator, p_type=ActorType.CLIENT)
-
-        if RFID_result:
-            time_opened = datetime.now()
-            controller.switch_led(LEDState.GREEN)
-
-            if not handle_opened(order_id=order_id, time_opened=time_opened, status_str=const.STATUS_DELIVERED,
-                                 controller=controller, communicator=communicator):
-                controller.blink_led(LEDState.RED, 5)
-                controller.switch_led(LEDState.OFF)
-                break
-
-        else:
-            controller.switch_led(LEDState.RED)
-            time.sleep(5)
-
-        controller.switch_led(LEDState.OFF)
 
 
 if __name__ == '__main__':
